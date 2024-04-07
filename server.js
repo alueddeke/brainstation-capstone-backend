31e60bbf04fd6fa8e75f14dd658336022e93f4c9
alueddeke <a.lueddeke@hotmail.com>
2024-04-07 12:51:00 -0400

    gpt functionality working, integrated front end

--- server.js
+++ server.js
@@ -2,28 +2,36 @@ const express = require("express");
 require("dotenv").config();
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const { Configuration, OpenAiApi } = require("openai");
+const OpenAI = require("openai");
 
+const PORT = process.env.PORT;
 const app = express();
 
 app.use(cors());
 //middleware parses each request into json format
 app.use(bodyParser.json());
 
-const configuration = new Configuration({
-  apikey: process.env.GPT_KEY,
+const openai = new OpenAI({
+  apiKey: process.env.GPT_KEY,
 });
 
-const openai = new OpenAiApi(configuration);
-
-app.post('/response' async (req, res) => {
-    const {prompt } = req.body
-    const complettion = await openai.chat.completions.create({
-        messages: [{role: "system", content: prompt}],
-        model="gpt-3.5-turbo",
-        max_tokens: 300,
-    })
+app.post("/response", async (req, res) => {
+  const { prompt } = req.body;
+  try {
+    const completion = await openai.chat.completions.create({
+      model: "gpt-3.5-turbo",
+      messages: [{ role: "user", content: prompt }],
+      max_tokens: 300,
+    });
     //may need to do just data or just choices to define what we actually want
-    res.send(completion.data.choices[0].content)
+    console.log(completion.choices[0].message.content);
+    res.send(completion.choices[0].message.content);
+  } catch (error) {
+    console.error("Error generating response:", error);
+    res.status(500).send("Error generating response");
+  }
+});
 
-})
+app.listen(PORT, () => {
+  console.log(`server is running at http://localhost:${PORT}`);
+});
